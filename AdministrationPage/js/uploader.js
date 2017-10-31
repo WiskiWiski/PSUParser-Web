f44622aa6cf369625bd79f2b3da9524138518c58
Andrew Yablonski <Dron2310@gmail.com>
2017-10-31 11:53:56 +0300

    Auto select fac/course by filename

--- AdministrationPage/js/uploader.js
+++ AdministrationPage/js/uploader.js
@@ -8,50 +8,59 @@ document.getElementById("document").addEventListener("change", handleFileSelect,
 var file;
 
 function sendFile() {
-	document.getElementById("output").innerHTML = ''
-	readFileInputEventAsArrayBuffer(file, post);
+    document.getElementById("output").innerHTML = '';
+    readFileInputEventAsArrayBuffer(file, post);
 }
+
+function parseFileName(filename) {
+    const facRegExp = /[A-z]+(?=\s*-\s*\d)/gm;
+    var facArr = filename.match(facRegExp);
+    if (facArr !== null) {
+        var fac = facArr[0];
+        if (fac !== undefined && fac.length >= 2 && fac.length <= 3) {
+            fac = fac.toLowerCase().trim();
+            selectFacByValue(fac);
+        }
+    }
+
+    const courseRegExp = /\d(?=\s*\.\s*)/gm;
+    const courseArr = filename.match(courseRegExp);
+    if (courseArr !== null) {
+        var course = courseArr[0];
+        if (course > 0 && course <= 5) {
+            selectCourseByValue(course);
+        }
+    }
+
+    function selectFacByValue(facValue) {
+        var facSelector = document.getElementById("fac_selector");
+        const list = facSelector.options;
+        for (var k = 0; k < list.length; k++) {
+            if (list[k].value === facValue) {
+                console.log('Select fac by filename: %s', list[k].text);
+                facSelector.value = list[k].value;
+                break;
+            }
+        }
+    }
+
+    function selectCourseByValue(courseValue) {
+        var courseSelector = document.getElementById("course_selector");
+        const list = courseSelector.options;
+        for (var k = 0; k < list.length; k++) {
+            if (list[k].value === '' + courseValue) {
+                console.log('Select course by filename: %d', k + 1);
+                courseSelector.value = k;
+                break;
+            }
+        }
+    }
+}
+
 
 function handleFileSelect(event) {
     file = event.target.files[0];
-
-/*
-    var xhr = new XMLHttpRequest();
-    xhr.onload = xhr.onerror = function () {
-        if (xhr.status === 200) {
-            alert(xhr.responseText);
-        } else {
-
-            alert(xhr.responseText);
-            console.log("error " + this.status);
-        }
-    };
-
-    xhr.open('POST', 'https://us-central1-psu-by.cloudfunctions.net/psu2', true);
-    xhr.setRequestHeader('Content-Type', 'application/octet-stream');
-
-    var reader = new FileReader();
-
-    reader.onload = function (loadEvent) {
-        var arrayBuffer = loadEvent.target.result;
-        console.log("result1: " + arrayBuffer);
-        //convert(arrayBuffer, callback);
-
-        xhr.send(toBuffer(arrayBuffer));
-    };
-
-    reader.readAsArrayBuffer(file);
-*/
-
-}
-
-function toBuffer(ab) {
-    var buf = new Buffer(ab.byteLength);
-    var view = new Uint8Array(ab);
-    for (var i = 0; i < buf.length; ++i) {
-        buf[i] = view[i];
-    }
-    return buf;
+    parseFileName(event.target.files[0].name);
 }
 
 function post(result) {
@@ -69,23 +78,23 @@ function post(result) {
     xhr.open("POST", "http://localhost:3000/", true);
     //xhr.open('POST', 'https://us-central1-psu-by.cloudfunctions.net/psu', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-	
-	var facElement = document.getElementById("fac_selector");
-	var courseElement = document.getElementById("course_selector");
-	
-	var fac = facElement.options[facElement.selectedIndex].value;
-	var course = courseElement.options[courseElement.selectedIndex].value;
-		
-	
-	const checkBox = document.getElementById("check_box");
-	let action = checkBox.checked?'show':'parse';
-	
-	var json = JSON.stringify({
-  		html: result.value,
-		action: action,
-  		fac: fac,
-  		course: course
-	});
+
+    var facElement = document.getElementById("fac_selector");
+    var courseElement = document.getElementById("course_selector");
+
+    var fac = facElement.options[facElement.selectedIndex].value;
+    var course = courseElement.options[courseElement.selectedIndex].value;
+
+
+    const checkBox = document.getElementById("check_box");
+    let action = checkBox.checked ? 'show' : 'parse';
+
+    var json = JSON.stringify({
+        html: result.value,
+        action: action,
+        fac: fac,
+        course: course
+    });
 
     xhr.send(json);
 }
@@ -108,12 +117,4 @@ function convert(arrayBuffer, callback) {
     mammoth.convertToHtml({arrayBuffer: arrayBuffer})
         .then(callback)
         .done();
-}
-
-function displayResult(rHtml) {
-
-    const r = result.value;
-    document.getElementById("output").innerHTML = r;
-
-    //fit_parse(document.getElementById("course_selector").value, html)
 }
